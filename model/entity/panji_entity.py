4c35d7be7f6db978b86fb8112f027c23d356828b
cmrhyq <cmrhyq@163.com>
2025-11-24 17:38:08 +0800

    更新环境配置参数

--- model/entity/panji_entity.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class PanjiSignEntity(object):
-    username: str = None
-    password: str = None
-    tenant_code: str = None
-    expire_time: int = 18000000
