a01b51ef986777308fdbf380dd1fc7da0405c9fb
cmrhyq <cmrhyq@163.com>
2025-12-09 17:33:39 +0800

    新增portal inner模块

--- base/api/services/portal_inner_service.py
+++ base/api/services/portal_inner_service.py
@@ -1,6 +1,8 @@
 import logging
+from typing import Dict, Any
 
 from base.api.services.base_service import BaseService
+from config import env_manager
 
 
 class PanJiPortalInnerService(BaseService):
@@ -11,7 +13,7 @@ class PanJiPortalInnerService(BaseService):
         初始化 Panji Portal InnerAPI 服务
 
         Args:
-            base_url: API 基础 URL，默认使用 JSONPlaceholder 官方地址
+            base_url: API 基础 URL
             logger: 日志记录器
         """
         super().__init__(
@@ -19,3 +21,21 @@ class PanJiPortalInnerService(BaseService):
             logger=logger
         )
         self.logger.info(f"Initializing PanJi InnerAPI Service with base_url: {self.base_url}")
+
+    def get_user_full_data(self) -> Dict[str, Any]:
+        """
+        获取用户全量数据
+
+        Returns:
+            Dict[str, Any]
+        """
+        self.logger.info(f"Getting User's Full Data")
+        # 本地调用时需要在portal后加入/server，其他情况则去除
+        url = "/portal/server/api/user/list"
+        headers = {
+            "apikey": env_manager.get_config().get("apikey"),
+            "tenantCode": env_manager.get_config().get("tenant_code"),
+            "x-app-id": "portal",
+        }
+        response = self.get(endpoint=url, headers=headers)
+        return response.json()
